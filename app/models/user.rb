c7ad18024b998ea2777bd967cefd5b86b832e3de
Amelia Haba <amelia.haba347@gmail.com>
2021-04-13 20:37:18 -0400

    add and update models

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+    has_secure_password 
+
+    has_many :characters
+
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+    
+end 
